f548423e92555a05173dcc71a4874e5be9d25fe6
Berkay Yükünç <berkayyukunc@Berkay-MacBook-Air-4.local>
2026-03-12 22:59:56 +0300

    final version

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ from custom_classes import Calculator
 
 if __name__ == "__main__":
     calc = Calculator()
-    print(f"Current Value: {calc._current_val}")
+    
+    # Example operation: (10 + 5) * 2 / 3
+    calc.add(10, 5)
+    calc.multiply(calc._current_val, 2)
+    calc.divide(calc._current_val, 3)
+    
+    print(f"Final Result of (10+5)*2/3 is: {calc._current_val}")
